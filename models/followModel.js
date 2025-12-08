2502bae9504492b508f8b8873d5482f393a638f2
clcok <ekfrha24@gmail.com>
2025-12-08 21:02:02 +0900

    feat: follow 확인 코드 추가

---
+++ models/followModel.js
@@ -0,0 +1,27 @@
+const { getPool } = require("../config/db.js"); // pool 초기화된 db.js 가져오기
+
+module.exports = {
+    isFollow: async (me, target) => {
+        const checkSql = `
+            SELECT COUNT(*) AS cnt
+            FROM FOLLOW
+            WHERE FOLLOWER_ID = :me
+              AND FOLLOWING_ID = :target
+        `;
+
+        const pool = getPool();
+        const conn = await pool.getConnection();
+
+        try {
+            const result = await conn.execute(checkSql, { me, target });
+
+            const count = result.rows[0][0];
+            return count > 0;   // true / false
+        } catch (err) {
+            console.error(err);
+            throw err;
+        } finally {
+            await conn.close();
+        }
+    }
+};
