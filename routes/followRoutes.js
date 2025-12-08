29a37685dad18ab0f337e44a825d91cf562e45c4
clcok <ekfrha24@gmail.com>
2025-12-08 21:01:40 +0900

    refactor: 내부 로직 받는 변수 수정

--- routes/followRoutes.js
+++ routes/followRoutes.js
@@ -1,19 +1,22 @@
 const {Router} = require('express');
 const router = Router();
-const followmodel = require('../models/follow.js');
+const followmodel = require('../models/followModel');
 
 // 1) /follow -> POST (팔로우 여부에 따라, 추가 및 삭제)
 router.post('', async (req, res) => {
     try {
-        const {followerId, followeeId} = req.body;
-        const result = await followmodel.toggleFollow(followerId, followeeId);
+        const {followerId, followingId} = req.body;
+        const result = await followmodel.isFollow(followerId, followingId);
 
-        if (result.following) {
-            // 팔로우 취소
+        if (result) {
+            console.log(result);
+            console.log('Unfollow logic here');
         } else {
+            console.log(result);
+            console.log('Follow logic here');
             // 팔로우 추가
         }
-        res.status(200).json({ result: true, following: result.following });
+        res.status(200).json({ result: true, following: result});
     } catch (error) {
         res.status(500).json({ result: false, message: 'Internal server error' });
     }
